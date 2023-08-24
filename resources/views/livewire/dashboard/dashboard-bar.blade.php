695b65ed18a078f5920be00ffb03ccf9e7dffba0
Marcio Renan <natulini@gmail.com>
2023-08-24 09:11:28 -0300

    Removido arrow de Eventos

--- resources/views/livewire/dashboard/dashboard-bar.blade.php
+++ resources/views/livewire/dashboard/dashboard-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="grid grid-cols-6 gap-4">
     <x-dashboard.card label="Faturamento" :value="$total['faturamento']" arrow="down-white" :selected="$active == 'faturamento'" />
-    <x-dashboard.card label="Eventos" :value="$total['events']" arrow="down" :selected="$active == 'eventos'" />
+    <x-dashboard.card label="Eventos" :value="$total['events']" :selected="$active == 'eventos'" />
     <x-dashboard.card label="MP" value="R$ 8.855,00" arrow="down" :selected="$active == 'mp'" />
     <x-dashboard.card label="MO" value="R$ 2.123,00" arrow="up-blue" :selected="$active == 'mo'" />
     <x-dashboard.card label="GD" value="R$ 123,00" arrow="up-blue" :selected="$active == 'gd'" />
